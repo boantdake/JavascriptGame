97b69352e5fa0dfe2850a36fe5614b52490e5926
Bo Dake <boantdake@gmail.com>
2018-02-27 23:12:54 -0500

    continuing javascript game
    simple game

--- js/app.js
+++ js/app.js
@@ -1,1 +1,48 @@
-$(document).foundation()
+$(document).ready(function(){
+    $("#playGame").on("click", function() {
+	
+		playGame();
+
+	})
+});
+
+
+function getComputerChoice(){
+  var choice = Math.floor(Math.random() * 3);
+  switch(choice){
+    case 1:
+      return 'rock';
+     	break;
+    case 2:
+      return 'paper';
+      break;
+    default:
+      return 'scissors';
+      break;
+   }
+}
+
+function determineWinner(userChoice,computerChoice){
+  if(userChoice == "rock" && computerChoice =="scissors"){
+    return "Human Wins";
+  }else if(userChoice == "paper" && computerChoice =="rock"){
+    return "Human Wins";
+  }else if(userChoice == "scissors" && computerChoice == "paper"){
+    return "Human Wins";
+  }else if(userChoice == computerChoice){
+    return "Tie";
+  }else{
+    return "Computer Wins";
+  }
+}
+function playGame(){
+  var userInput = prompt('Rock,Paper,Scissors?');
+  userInput = userInput.toLowerCase();
+  var userChoice = userInput;
+  var computerChoice = getComputerChoice();
+  alert('You threw: ' + userChoice);
+  alert('The computer threw: ' + computerChoice);
+  
+  alert(determineWinner(userChoice,computerChoice));
+  
+}
